41a9302c51d55d38a4942df8fdf45f6e9a90e0ac
kvabhinav <abhinavkv888@gmail.com>
2023-08-04 16:38:44 +0530

    technology page for tablet screen

--- src/components/TechnologyComponent.jsx
+++ src/components/TechnologyComponent.jsx
@@ -3,8 +3,8 @@ import state from "../state"
 export default function TechnologyComponent(props) {
     return (
         <div className="w-screen h-screen pt-24 lg:pt-40 ">
-            <div className="lg:pl-32 lg:pt-10">
-                <h2 className="text-white font-barlow text-base lg:text-[28px] tracking-[2.7px] lg:tracking-[4.72px] text-center lg:text-left"><span className="text-white/20 font-bold">03 </span>SPACE LAUNCH 101</h2>
+            <div className="md:pl-8 md:pt-8 lg:pl-32 lg:pt-10">
+                <h2 className="text-white font-barlow text-base lg:text-[28px] tracking-[2.7px] lg:tracking-[4.72px] text-center md:text-left"><span className="text-white/20 font-bold">03 </span>SPACE LAUNCH 101</h2>
             </div>
             <div className="flex flex-col-reverse lg:flex-row lg:justify-center lg:items-center w-screen lg:h-[87%] text-center lg:text-left">
                 <div className="lg:flex lg:w-8/12 lg:px-32 lg:gap-x-10 lg:h-80">
@@ -20,9 +20,9 @@ export default function TechnologyComponent(props) {
                         </div>
                     </div>
                     <div>
-                        <h5 className="font-barlow text-[14px] lg:text-base tracking-[2.36px] lg:tracking-2.7px text-[#D0D6F9]">THE TERMINOLOGY...</h5>
-                        <h1 className="text-white font-bellefair text-[24px] lg:text-[56px]">{props.data.step}</h1>
-                        <p className="text-[#D0D6F9] text-[15px] lg:text-[18px] leading-[25px] lg:leading-8 px-8 lg:px-0 lg:pr-32 pt-3 lg:pt-0">{props.data.description}</p>
+                        <h5 className="font-barlow text-[14px] md:text-base lg:text-base tracking-[2.36px] lg:tracking-2.7px text-[#D0D6F9]">THE TERMINOLOGY...</h5>
+                        <h1 className="text-white font-bellefair text-[24px] md:text-[40px] lg:text-[56px]">{props.data.step}</h1>
+                        <p className="text-[#D0D6F9] text-[15px] md:text-[16px] lg:text-[18px] leading-[25px] md:leading-7 lg:leading-8 px-8 md:px-40 lg:px-0 lg:pr-32 pt-3 lg:pt-0">{props.data.description}</p>
                     </div>
                 </div>
                 <div className="lg:w-4/12 mt-6 mb-2 lg:mt-0 lg:mb-0">
